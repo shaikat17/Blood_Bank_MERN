3f386572d9e5c7b6c68949242492b834d97c01ef
shaikat17 <shaikatpal56@gmail.com>
2023-10-14 22:34:16 +0600

    Route added. Working on login page.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import Home from './pages/Home.jsx';
+import Login from './pages/auth/Login.jsx';
+import Register from './pages/auth/Register.jsx';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ client/src/pages/auth/Login.jsx
@@ -0,0 +1,16 @@
+import banner1 from "../../assets/images/banner1.jpg"
+
+const Login = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-">
+          <img src={banner1} alt="banner1" className="banner1" />
+        </div>
+        <div className="col-md-4">Login</div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
