c8944a21306656e8c40998dd1c07f245ee251f6c
shaikat17 <shaikatpal56@gmail.com>
2023-10-14 23:25:19 +0600

    Login and Registration page created.

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,13 +1,19 @@
 import banner1 from "../../assets/images/banner1.jpg"
-
+import Form from "../../components/shared/form/From"
 const Login = () => {
   return (
     <>
       <div className="row">
-        <div className="col-md-">
+        <div className="col-md-8">
           <img src={banner1} alt="banner1" className="banner1" />
         </div>
-        <div className="col-md-4">Login</div>
+        <div className="col-md-4 form-container">
+        <Form
+              formTitle={"Login Page"}
+              submitBtn={"Login"}
+              formType={"login"}
+            />
+        </div>
       </div>
     </>
   );
--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,11 +1,35 @@
-
+import React from "react";
+import Form from "../../components/shared/form/From";
+// import { useSelector } from "react-redux";
+// import Spinner from "../../components/shared/Spinner";
+import banner2 from "../../assets/images/banner2.jpg"
 
 const Register = () => {
-    return (
-        <div>
-            Register
+//   const { loading, error } = useSelector((state) => state.auth);
+const error = false;
+const loading = false;
+  return (
+    <>
+      {error && <span>{alert(error)}</span>}
+      {loading ? (
+        // <Spinner />
+        console.log("Hello world")
+      ) : (
+        <div className="row g-0">
+          <div className="col-md-8">
+            <img src={banner2} alt="registerImage" className="banner2" />
+          </div>
+          <div className="col-md-4 form-container">
+            <Form
+              formTitle={"Register"}
+              submitBtn={"Register"}
+              formType={"register"}
+            />
+          </div>
         </div>
-    );
+      )}
+    </>
+  );
 };
 
 export default Register;
